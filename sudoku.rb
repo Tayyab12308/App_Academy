1765a6c8c9b2d243da7134221f51181f0ff58a34
Emre <emre.ersolmaz@gmail.com>
2019-08-15 19:01:29 -0400

    We could not finish up with this one! Brain dead

--- sudoku.rb
+++ sudoku.rb
@@ -1,19 +1,24 @@
 require_relative "board"
 require 'colorize'
 
+require "byebug"
+
 puts "Only contractors write code this bad.".yellow
 
 class SudokuGame
   def self.from_file(filename)
     board = Board.from_file(filename)
     self.new(board)
+
   end
 
   def initialize(board)
-    @board = [[]]
+    
+    @board = board
+    
   end
 
-  def method_missing(method_name, *args)
+  def method_name?(method_name, *args)
     if method_name =~ /val/
       Integer(1)
     else
@@ -31,8 +36,7 @@ class SudokuGame
       begin
         pos = parse_pos(gets.chomp)
       rescue
-        # TODO: Google how to print the error that happened inside of a rescue statement.
-        puts "Invalid position entered (did you use a comma?)"
+        raise "Invalid position entered (did you use a comma?)"
         puts ""
 
         pos = nil
@@ -85,3 +89,4 @@ end
 
 
 game = SudokuGame.from_file("puzzles/sudoku1.txt")
+game.run
